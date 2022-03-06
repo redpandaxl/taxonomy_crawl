df9d19fe08335edd2061aeffc4a3c4bedeb7ec7c
Chris H <redpandaxlbusiness@gmail.com>
2022-03-05 22:42:33 -0800

    working traversal

--- crawl.go
+++ crawl.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"github.com/gocarina/gocsv"
 	"os"
@@ -20,57 +19,25 @@ func main() {
 	}
 	defer taxonomy.Close()
 
-	segments := []*segment{}
+	var segments []segment
 
 	if err := gocsv.UnmarshalFile(taxonomy, &segments); err != nil { // Load clients from file
 		panic(err)
-	}
-
-	for _, segment := range segments {
-
-		fmt.Print(segment.Name, "\n\n")
 
 	}
+
+	printTree(segments, "null", 0)
 
 }
 
-// ReadCsv accepts a file and returns its content as a multi-dimension type
-// with lines and each column. Only parses to string type.
-func ReadCsv(filename string) ([][]string, error) {
-
-	// Open CSV file
-	f, err := os.Open(filename)
-	if err != nil {
-		return [][]string{}, err
+func printTree(tbl []segment, parent string, depth int) {
+	for _, r := range tbl {
+		if r.ParentID == parent {
+			for i := 0; i < depth; i++ {
+				fmt.Print("--")
+			}
+			fmt.Print(r.Name, "\n\n")
+			printTree(tbl, r.ID, depth+1)
+		}
 	}
-	defer f.Close()
-
-	// Read File into a Variable
-	lines, err := csv.NewReader(f).ReadAll()
-	if err != nil {
-		return [][]string{}, err
-	}
-
-	return lines, nil
 }
-
-//func printTree(tbl, parent string, depth int) {
-//	for _, r := range tbl {
-//		if r.ParentID == parent {
-//			for i := 0; i < depth; i++ {
-//				fmt.Print("--")
-//			}
-//			fmt.Print(r.Name, "\n\n")
-//			printTree(tbl, r.ID, depth+1)
-//		}
-//	}
-//}
-
-//func printTree(tbl, parent string, depth int) {
-//	if tbl.ParentID == "null" {
-//		for i := 0; i < depth; i++ {
-//			fmt.Print("--")
-//		}
-//		fmt.Print(tbl.Name, "\n\n")
-//		//printTree(segment, segment.ID, depth+1)
-//	}
